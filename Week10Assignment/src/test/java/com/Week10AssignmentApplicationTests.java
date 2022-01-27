75b86f5cd02fdf1b93c9b2d52db435f1d98b4398
praveenkorada <praveenkumar0143p@gmail.com>
2022-01-27 14:54:15 +0530

    file added

---
+++ Week10Assignment/src/test/java/com/Week10AssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week10AssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
